ac72b0b3527e967a5037cf578b380c180123d41b
nile16 <nils.leandersson@gmail.com>
2017-01-29 18:19:31 +0100

    Added function for the last 10 murders

--- readdb.py
+++ readdb.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
-from flask_cors import CORS, cross_origin #Used to allow http requsets from other servers than the one this code runs on
+from flask import Flask
+from flask_cors import CORS #Used to allow http requsets from other servers than the one this code runs on
 import MySQLdb #Used to communicate with MySQL database
-import time
 import json #Used to format data to JSON format
 
 app = Flask(__name__)
@@ -25,6 +24,22 @@ def byarea():
         y.append(x)
     return(json.dumps(y))
 
-
+@app.route('/mord')
+def mord():
+    db = MySQLdb.connect(host="localhost", user='root', passwd='apa', db='police', use_unicode=True, charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("select * from events where title like '%mord%' or summary like '%mord%' order by time desc limit 10;")
+    result=cursor.fetchall()
+    y=[]
+    for data in result:
+        x=[]
+        x.append(data[0])                 #Unix time stamp
+        x.append(data[1].encode('utf-8')) #Title
+        x.append(data[2].encode('utf-8')) #Summary
+        x.append(data[3].encode('utf-8')) #County
+        x.append(data[4].encode('utf-8')) #Link
+        y.append(x)
+    return(json.dumps(y))
+	
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=1203)
